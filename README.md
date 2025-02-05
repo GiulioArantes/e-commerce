e69b4c3dd3c83ccb53210976e2ef97b5aebf76fb
GiulioArantes <giulio.arantes@icloud.com>
2025-02-05 11:40:30 -0300

    chore(repository): 🔧 update prettier config

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # E-commerce Fictício | Desafio 100 Dias de Código 🚀
 
 ## 📖 Sobre o projeto
+
 Este é um projeto de e-commerce fictício criado como parte do desafio **#100DaysOfCode**, com foco no aprendizado e evolução em front-end. O objetivo é, ao final do desafio, desenvolver um site moderno e funcional, próximo ao padrão de um e-commerce profissional, utilizando tecnologias como HTML, CSS, JavaScript e futuramente React. O projeto também serve como portfólio para demonstrar minhas habilidades em desenvolvimento web.
 
 ---
 
 ## 🛠️ Tecnologias utilizadas
+
 Até agora, as tecnologias e ferramentas usadas no projeto são:
+
 - **HTML5**: Para a estrutura semântica do site.
 - **CSS3**: Para o design e responsividade.
 - **JavaScript**: Para funcionalidades interativas.
@@ -17,9 +20,11 @@ Conforme avanço no desafio, serão integradas novas tecnologias e bibliotecas/f
 ---
 
 ## 📈 Progresso do desafio
+
 O projeto será atualizado continuamente, refletindo as melhorias e o aprendizado durante o desafio.
 
 **Etapas concluídas até agora:**
+
 - [x] Estrutura básica em HTML.
 - [x] Design inicial com CSS.
 - [x] Lógica básica com JavaScript.
@@ -27,6 +32,7 @@ O projeto será atualizado continuamente, refletindo as melhorias e o aprendizad
 - [x] Sistema de busca por nome e categoria.
 
 **Próximos passos:**
+
 - [ ] Implementar responsividade completa.
 - [ ] Adicionar sistema de filtros (preço, categoria, etc.).
 - [ ] Desenvolver o carrinho de compras (simulado).
@@ -37,11 +43,14 @@ O projeto será atualizado continuamente, refletindo as melhorias e o aprendizad
 ---
 
 ## 🔧 Como executar o projeto
+
 1. Clone este repositório:
    ```bash
    git clone https://github.com/seu-usuario/e-commerce-desafio-100-dias.git
+   ```
 
 ## ✨ Funcionalidades planejadas
+
 - Exibição de produtos dinâmicos.
 - Sistema de busca e filtros.
 - Carrinho de compras (simulado).
@@ -53,18 +62,22 @@ O projeto será atualizado continuamente, refletindo as melhorias e o aprendizad
 ---
 
 ## 🤝 Contribuição
+
 Contribuições são bem-vindas! Siga os passos abaixo para colaborar:
 
 1. Faça um fork deste repositório.
 2. Crie uma branch com suas alterações:
-    ```bash
-    git checkout -b minha-feature `
+   ```bash
+   git checkout -b minha-feature `
+   ```
 3. Faça o commit:
-    ```bash
-    git commit -m "Adicionei uma nova feature"
+   ```bash
+   git commit -m "Adicionei uma nova feature"
+   ```
 4. Envie suas alterações:
-    ```bash
-    git push origin minha-feature
+   ```bash
+   git push origin minha-feature
+   ```
 5. Abra um pull request.
 
 ## Reportando Bugs ou Sugerindo Melhorias:
@@ -74,4 +87,5 @@ Contribuições são bem-vindas! Siga os passos abaixo para colaborar:
 ---
 
 ## 📄 Licença
+
 Este projeto está sob a licença MIT. Veja o arquivo [LICENSE](./LICENSE) para mais detalhes.
--- js/script.js
+++ js/script.js
@@ -1,187 +1,189 @@
 let products = [];
-const displayProducts = document.getElementById("display-products");
-const modal = document.querySelector("#modal");
-const modalDescription = document.querySelector("#modal-description");
-const closeModalBtn = document.querySelector("#close-modal-btn");
-const searchDiv = document.querySelector("#search-div");
-const search = document.querySelector("#search");
-const searchBtn = document.querySelector(".search-button");
-const cartDiv = document.querySelector("#cart-div");
-const modalCart = document.querySelector("#cart-modal");
+const displayProducts = document.getElementById('display-products');
+const modal = document.querySelector('#modal');
+const modalDescription = document.querySelector('#modal-description');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const searchDiv = document.querySelector('#search-div');
+const search = document.querySelector('#search');
+const searchBtn = document.querySelector('.search-button');
+const cartDiv = document.querySelector('#cart-div');
+const modalCart = document.querySelector('#cart-modal');
 
 const toggleElement = (element, displayStyle) => {
-    element.style.display = displayStyle;
-}
+  element.style.display = displayStyle;
+};
 
 async function fetchProducts() {
-    const loading = document.getElementById("loading");
-    const errorMessage = document.getElementById("error-message");
+  const loading = document.getElementById('loading');
+  const errorMessage = document.getElementById('error-message');
 
-    toggleElement(loading, "block");
-    toggleElement(errorMessage, "none");
+  toggleElement(loading, 'block');
+  toggleElement(errorMessage, 'none');
 
-    try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) {
-            throw new Error(`Erro ao carregar os produtos. Status: ${response.status}`);
-        }
-
-        products = await response.json();
-        renderProduct(products);
-    } catch (error) {
-        errorMessage.textContent = `Ocorreu um erro ${error.message}`;
-        toggleElement(errorMessage, "block");
-    } finally {
-        toggleElement(loading, "none");
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) {
+      throw new Error(
+        `Erro ao carregar os produtos. Status: ${response.status}`
+      );
     }
+
+    products = await response.json();
+    renderProduct(products);
+  } catch (error) {
+    errorMessage.textContent = `Ocorreu um erro ${error.message}`;
+    toggleElement(errorMessage, 'block');
+  } finally {
+    toggleElement(loading, 'none');
+  }
 }
 
 const createElement = (tag, className, content, attributes = {}) => {
-    const element = document.createElement(tag);
-    if (className) element.classList.add(className);
-    if (content) element.textContent = content;
-    for (const [key, value] of Object.entries(attributes)) {
-        element.setAttribute(key, value);
-    }
-    return element;
-}
+  const element = document.createElement(tag);
+  if (className) element.classList.add(className);
+  if (content) element.textContent = content;
+  for (const [key, value] of Object.entries(attributes)) {
+    element.setAttribute(key, value);
+  }
+  return element;
+};
 
 function renderProduct(products) {
-    displayProducts.innerHTML = "";
-    products.forEach(product => {
-        //Imagem
-        const img = document.createElement("img");
-        img.classList.add("product-img");
-        img.setAttribute("src", product.image);
-        img.setAttribute("alt", product.title);
-        const figure = document.createElement("figure");
-        figure.appendChild(img);
+  displayProducts.innerHTML = '';
+  products.forEach((product) => {
+    //Imagem
+    const img = document.createElement('img');
+    img.classList.add('product-img');
+    img.setAttribute('src', product.image);
+    img.setAttribute('alt', product.title);
+    const figure = document.createElement('figure');
+    figure.appendChild(img);
 
-        //Título
-        const h3 = document.createElement("h3");
-        h3.setAttribute("title", product.title);
-        h3.textContent = product.title;
+    //Título
+    const h3 = document.createElement('h3');
+    h3.setAttribute('title', product.title);
+    h3.textContent = product.title;
 
-        //Categoria
-        const pCategory = document.createElement("p");
-        pCategory.classList.add("category");
-        pCategory.textContent = product.category;
+    //Categoria
+    const pCategory = document.createElement('p');
+    pCategory.classList.add('category');
+    pCategory.textContent = product.category;
 
-        //Preço
-        const pPrice = document.createElement("p");
-        pPrice.classList.add("price");
-        pPrice.textContent = `R$ ${product.price}`;
+    //Preço
+    const pPrice = document.createElement('p');
+    pPrice.classList.add('price');
+    pPrice.textContent = `R$ ${product.price}`;
 
-        //Botões detalhe - carrinho
-        const openModalBtn = document.createElement("button");
-        openModalBtn.classList.add("btn-product");
-        openModalBtn.textContent = "Detalhes";
+    //Botões detalhe - carrinho
+    const openModalBtn = document.createElement('button');
+    openModalBtn.classList.add('btn-product');
+    openModalBtn.textContent = 'Detalhes';
 
-        openModalBtn.addEventListener("click", () => {
-            openModal(product.description);
-        })
-
-        const divCart = document.createElement("div");
-        divCart.classList.add("div-cart");
-        const imgCart = document.createElement("img");
-        imgCart.classList.add("img-cart");
-        imgCart.src = "assets/cart.svg"
-        imgCart.alt = "Adicionar ao carrinho"
-        divCart.appendChild(imgCart);
-        divCart.addEventListener("click", () => {
-            const productInfo = {
-                title: product.title,
-                price: product.price,
-            };
-            storeInfo(productInfo);
-        });
-
-        // Container para os botões 
-        const cardBtns = document.createElement("div");
-        cardBtns.classList.add("card-btns");
-        cardBtns.append(divCart, openModalBtn)
-
-        const card = document.createElement("article");
-        card.id = product.id;
-        card.classList.add("card");
-
-        //Agrupamento
-        card.append(figure, h3, pCategory, pPrice, cardBtns);
-        // card.appendChild(pDescription);
-        displayProducts.appendChild(card);
+    openModalBtn.addEventListener('click', () => {
+      openModal(product.description);
     });
+
+    const divCart = document.createElement('div');
+    divCart.classList.add('div-cart');
+    const imgCart = document.createElement('img');
+    imgCart.classList.add('img-cart');
+    imgCart.src = 'assets/cart.svg';
+    imgCart.alt = 'Adicionar ao carrinho';
+    divCart.appendChild(imgCart);
+    divCart.addEventListener('click', () => {
+      const productInfo = {
+        title: product.title,
+        price: product.price,
+      };
+      storeInfo(productInfo);
+    });
+
+    // Container para os botões
+    const cardBtns = document.createElement('div');
+    cardBtns.classList.add('card-btns');
+    cardBtns.append(divCart, openModalBtn);
+
+    const card = document.createElement('article');
+    card.id = product.id;
+    card.classList.add('card');
+
+    //Agrupamento
+    card.append(figure, h3, pCategory, pPrice, cardBtns);
+    // card.appendChild(pDescription);
+    displayProducts.appendChild(card);
+  });
 }
 // Descrição
 
 const searchProduct = () => {
-    const searchTerm = search.value.toLowerCase();
-    const cards = document.querySelectorAll(".card");
+  const searchTerm = search.value.toLowerCase();
+  const cards = document.querySelectorAll('.card');
 
-    let found = false;
+  let found = false;
 
-    cards.forEach(card => {
-        const title = card.querySelector("h3").textContent.toLowerCase();
-        const category = card.querySelector(".category").textContent.toLowerCase();
+  cards.forEach((card) => {
+    const title = card.querySelector('h3').textContent.toLowerCase();
+    const category = card.querySelector('.category').textContent.toLowerCase();
 
-        if (title.includes(searchTerm) || category.includes(searchTerm)) {
-            card.classList.remove("hidden");
-            found = true;
-        } else {
-            card.classList.add("hidden");
-        }
-    })
-
-    if (!found) {
-        cards.forEach(card => card.classList.remove("hidden"));
-        alert(`Produto não encontrado. Tente novamente`)
+    if (title.includes(searchTerm) || category.includes(searchTerm)) {
+      card.classList.remove('hidden');
+      found = true;
+    } else {
+      card.classList.add('hidden');
     }
+  });
+
+  if (!found) {
+    cards.forEach((card) => card.classList.remove('hidden'));
+    alert(`Produto não encontrado. Tente novamente`);
+  }
 };
 
-searchBtn.addEventListener("click", searchProduct);
-search.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        searchProduct();
-    }
+searchBtn.addEventListener('click', searchProduct);
+search.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    searchProduct();
+  }
 });
 
 function storeInfo(product) {
-    let savedProduct = JSON.parse(localStorage.getItem('savedProducts')) || [];
-    if (savedProduct[product.title]) {
-        savedProduct[product.title].quantity += 1;
-    }
-    savedProduct[product.title] = {
-        quantity: 1,
-        price: product.price
-    }
-    localStorage.setItem("savedProducts", JSON.stringify(savedProduct));
-    console.log("produto salvo:", product);
-    alert(`Você quer comprar o item: ${product.title} - R$ ${product.price}`);
+  let savedProduct = JSON.parse(localStorage.getItem('savedProducts')) || [];
+  if (savedProduct[product.title]) {
+    savedProduct[product.title].quantity += 1;
+  }
+  savedProduct[product.title] = {
+    quantity: 1,
+    price: product.price,
+  };
+  localStorage.setItem('savedProducts', JSON.stringify(savedProduct));
+  console.log('produto salvo:', product);
+  alert(`Você quer comprar o item: ${product.title} - R$ ${product.price}`);
 }
 
 let cart = [];
 function showCart(product) {
-    cart.push(product);
-    localStorage.setItem("cart", JSONN.stringify(cart));
-    alert(`${product.title} adicionado ao carrinho!`);
+  cart.push(product);
+  localStorage.setItem('cart', JSONN.stringify(cart));
+  alert(`${product.title} adicionado ao carrinho!`);
 }
 
 function openCart() {
-    modalCart.style.display = "flex";
+  modalCart.style.display = 'flex';
 }
 
 function openModal(description) {
-    modalDescription.textContent = description;
-    modal.style.display = "flex";
+  modalDescription.textContent = description;
+  modal.style.display = 'flex';
 }
 
-closeModalBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-})
+closeModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
-window.addEventListener("click", (event) => {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-})
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
 
 fetchProducts();
